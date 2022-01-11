4eff598ce177566da860088dbc815c7dcf67c274
kawaremu <boucah@hotmail.com>
2022-01-11 13:09:29 +0000

    Corrected mistakes

--- Unit 1/1_string_lists.py
+++
@@ -1,7 +0,0 @@
-even_list = [item for item in range(1,299) if item%2==0]
-
-print(len(even_list))
-
-print([i**2 for i in even_list])
-
-print(57 in even_list)
--- Unit 1/3_exceptions.py
+++
@@ -1,7 +0,0 @@
-a = 12
-s = "hello"
-
-try:
-  print(a+s)
-except Exception as ex:
-  print('{} Exception has occured.'.format(type(ex).__name__))
