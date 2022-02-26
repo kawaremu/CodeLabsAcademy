8fa0b216a783ca354f984622dabbaf9568233588
Ahlem <boucah@hotmail.com>
2022-02-27 00:16:43 +0100

    Finished linear algebra homework

--- Unit 1/Homeworks/6_linear_algebra.py
+++ Unit 1/Homeworks/6_linear_algebra.py
@@ -1,15 +1,37 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+from numpy.linalg import norm
 
 
+# You are given a colored image, read the image and apply the following operations to it:
+# Transpose.
+# image = cv2.imread('splash.jpg',0)
+# T = image.T.astype(np.uint8)
+# plt.imshow(T,cmap='gray')
+# plt.show()
 
-image = cv2.imread('splash.jpg')
+# Check if the matrix is one or many of the special types we have seen (diagonal, triagonal...).
 
-transpose = image.T.astype(np.uint8)
-plt.imshow(transpose)
-plt.show()
+mat1 = np.matrix([[1,0,0],
+                 [0,2,0],
+                 [0,0,3]
+                 ])
+is_diag_mat = np.allclose(mat1, np.diag(np.diag(mat1)))
+
+if is_diag_mat:
+  print('Matrix is diagonal')
+else:
+  print('Matrix is not diagonal')
 
 
-test_matrix = np.array([7,4,1,2,5,4,8,7,4,5,2,1,4,5])
+# Choose a column vector and calculate both norms that we've seen
+mat = np.arange(0,25).reshape(5,5)
+X = mat[:,:1]
 
+l1 = norm(X)
+
+print(l1)
+
+l2 = norm(X,1)
+print(l2)
